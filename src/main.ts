0b2bfcf8e57997641b98d8c64a98b7d9e197662a
jonattfin <jonattfin@outlook.com>
2022-07-02 14:16:44 +0300

    added the sensors endpoints and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CanaryBird')
+    .setDescription('The CanaryBird API description')
+    .setVersion('1.0')
+    .addTag('sensors')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
